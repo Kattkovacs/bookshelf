88ec00a1f92d078d4c78ea36136b43ff077654ae
Katalin Kovács <katalin.kovacs@innosw.hu>
2022-01-14 18:46:24 +0100

    add main image and input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import './App.css';
+import { InputGroup, Input, Button } from 'reactstrap';
 
 function App() {
+  const mainImage = () => {
+    return (
+      <div className='main-image d-flex justify-content-center align-items-center flex-column'>
+        {/* Overlay */}
+        <h1 className='display-2 text-center text-white mb-3' >
+          Online Bookshelf
+        </h1>
+        <div style={{ width: '60%' }}>
+          <InputGroup className='mb-3' size='lg'>
+            <Input placeholder='Book search' />
+            <Button color='secondary'>
+              <i className='fas fa-search'></i>
+            </Button>
+          </InputGroup>
+        </div>
+      </div>
+    )
+  }
   return (
     <div>
-      Start
+      {mainImage()}
     </div>
   );
 }
